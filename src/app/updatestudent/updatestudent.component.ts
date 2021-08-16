26df6a7aa126302f4e6d2149da7303b89dcf6562
satheeshmkm <satheeshmkm@gmail.com>
2021-08-16 08:32:04 +0530

    Day 06 -PUT call Update/Edit changes

--- src/app/updatestudent/updatestudent.component.ts
+++ src/app/updatestudent/updatestudent.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import { StudentsService } from '../students.service';
 
 @Component({
   selector: 'app-updatestudent',
@@ -9,19 +10,46 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./updatestudent.component.css']
 })
 export class UpdatestudentComponent implements OnInit {
-  id:any;
-  constructor(private http:HttpClient, private currentActiveRoute:ActivatedRoute, private router:Router) { }
+  id: any;
+  allStudents: any = [];
+  displayStudentInfo: any = {};
+  existed: boolean = false;
+  updatedStuInfo: any = {};
+
+
+  constructor(private http: HttpClient, private currentActiveRoute: ActivatedRoute, private router: Router, private stuSrvc: StudentsService) { }
 
   ngOnInit(): void {
-    this.currentActiveRoute.params.subscribe((params)=>{
-      this.id=+params['id'];
+    this.currentActiveRoute.params.subscribe((params) => {
+      this.id = +params['id']; // TO habdle it being undefined
       console.log(this.id);
-
     });
-
+    this.stuSrvc.getAllStudents().subscribe((studentsObtained) => {
+      this.allStudents = studentsObtained;
+      for (var i = 0; i < this.allStudents.length; i++) {
+        if (parseInt(this.allStudents[i].id) == this.id) {
+          this.displayStudentInfo = this.allStudents[i];
+          this.existed = true;
+          break;
+        } else {
+          this.existed = false;
+        }
+      }
+    })
   }
 
-  updateStudent(updateStuFrm:NgForm){
+  updateStudent(updateStuFrm: NgForm) {
+    if (updateStuFrm.invalid) {
+      return;
+    }
+    this.updatedStuInfo = {
+      "name": updateStuFrm.value.stuName,
+      "email": updateStuFrm.value.stuEmail
+    };
+    const url = `${'http://localhost:4444/students'}/${this.id}`
+    this.http.put(url, this.updatedStuInfo).subscribe(() => {
+      this.router.navigate(['/'])
+    })
 
   }
 
