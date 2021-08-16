45c93b4c75c40a73e1c6dfc9f8948dfffd84e65e
satheeshmkm <satheeshmkm@gmail.com>
2021-08-16 08:58:09 +0530

    Day 06 - Delete call Delete changes

--- src/app/studentmanagement/studentmanagement.component.ts
+++ src/app/studentmanagement/studentmanagement.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentsService } from '../students.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-studentmanagement',
@@ -8,17 +9,27 @@ import { StudentsService } from '../students.service';
 })
 export class StudentmanagementComponent implements OnInit {
 
-  students:any=[];
-  constructor(private stuSvc:StudentsService) { }
+  students: any = [];
+
+  constructor(private stuSvc: StudentsService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getStudents();
   }
 
-  getStudents(){
-    this.stuSvc.getAllStudents().subscribe((studentsResp)=>{
-      this.students=studentsResp;
+  getStudents() {
+    this.stuSvc.getAllStudents().subscribe((studentsResp) => {
+      this.students = studentsResp;
     });
+  }
+
+  deleteStudent(id: any) {
+    if (confirm('Are you sure to delete')) {
+      const url = `${'http://localhost:4444/students'}/${id}`;
+      this.http.delete(url).subscribe(() => {
+        this.getStudents();
+      });
+    }
   }
 
 }
